58ef59970aa8fc0d46f1d3fbc672f382e575e856
Gustavo Sousa <gfsousa42@gmail.com>
2023-07-31 15:45:59 -0300

    feat: present_menu method

--- main.py
+++ main.py
@@ -32,13 +32,37 @@ class LibraryCRUD:
         self.cursor.execute("select * from livros")
         print(self.cursor.fetchall())
 
+    def choose_action(self, choice):
+        if choice == "1":
+            self.show_all_books()
+        elif choice == "2":
+            self.add_book("name", "some writer")
+        elif choice == "3":
+            self.add_book("name", "some writer")
+        # elif choice == "4":
+        #     self.update_book("name", "some writer")
+        # elif choice == "5":
+        #     self.search_book("name", "some writer")
+        elif choice == "6":
+            exit()
+ 
+    def present_menu(self):
+        print("\nWelcome to the Library. What do you intent?\n")
+        print("1 - show all books")
+        print("2 - Add a new book with basic information")
+        print("2 - Add a new book with complete information")
+        print("3 - Reenter information for an existing book")
+        print("4 - Delete a book by id")
+        print("5 - Search for a book")
+        print("6 - Exit")
+        choice = input("Type the desirable number: ")
+        self.choose_action(choice)
+
 # Main
 if __name__ == "__main__":
     try:
         with LibraryCRUD() as library:
-            print("Library created with success")
-            library.show_all_books() 
-            library.add_book("The hobbit", "JRR Tolkien")
-            library.show_all_books() 
+            while (True):
+                library.present_menu()
     except Exception as e:
         print("error: ", str(e))
